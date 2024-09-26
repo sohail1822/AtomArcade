344ae806caf899a9eb455de703e46ab77721aef7
Sohail Shaikh <99899172+sohail1822@users.noreply.github.com>
2024-09-26 16:52:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,17 +2,19 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config, { isServer }) {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: "node_modules/@rdkit/rdkit/dist/RDKit_minimal.wasm",
-            to: "static/chunks",
-          },
-        ],
-      }),
-    );
+  webpack(config, { isServer, dev }) {
+    if (dev || !isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: "node_modules/@rdkit/rdkit/dist/RDKit_minimal.wasm",
+              to: "static/chunks",
+            },
+          ],
+        }),
+      );
+    }
 
     if (!isServer) {
       config.resolve.fallback = {
